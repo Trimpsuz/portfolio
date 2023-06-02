d95378744f88bd4b6cd786df3f375d8c3c20b0d2
Trimpsuz <mail@trimpsuz.xyz>
2023-06-02 20:28:14 +0300

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../styles/global.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { MantineProvider } from '@mantine/core';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colors: {
+          primary: ['#fdfdfd'],
+          secondary: ['#181818', '#1f1f1f', 'rgba(255, 255, 255, 0.1)'],
+          primaryButton: ['#7d34e7'],
+          secondaryButton: ['#fdfdfd'],
+          accent: ['#e534af'],
+        },
+
+        shadows: {
+          md: '1px 1px 3px rgba(0, 0, 0, .25)',
+          xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+        },
+
+        headings: {
+          fontFamily: 'Source Sans Pro, Sans-Serif',
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default App;
